9ca5dc0498a8787de1c20552ddb726cfb8dfc262
DwayneHua <DwayneHua@gmail.com>
2012-02-11 15:41:17 +0000

    initial com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PongElo::Application.config.secret_token = '49b1d659549adb4ed3cf60a4cdd719c4083d11197fdf58c612faaa185e652b4740ce595747aa708ef07b1a48a5b99277c12a4527a7adb274bd95580d481da619'
